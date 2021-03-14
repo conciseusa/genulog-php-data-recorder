4fe54008f6c2f43e04609eab808dfa3ceb37897e
conciseusa <walter@conciseusa.com>
2021-03-13 18:29:19 -0800

    Create remote-data-config.php

---
+++ remote-data-config.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'upkey' => 'super_secret_key', // a value other then stationId that can be changed to provide a little bit of security
+    'relayUrl' => '',
+    'testUrl' => '',
+);
