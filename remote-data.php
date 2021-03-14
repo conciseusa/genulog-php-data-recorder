098b3e25254c53ad2cf55372f3823988e673436a
conciseusa <walter@conciseusa.com>
2021-03-13 19:02:22 -0800

    Move StationId to config file

--- remote-data.php
+++ remote-data.php
@@ -15,7 +15,7 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && (trim(trim($_POST["upkey"],"'"),'"
 
   if ($configs['relayUrl']) { // relay to next data collection service
     $data = json_decode($json, true);
-    $data["stationId"] = 'CCW1';
+    $data["stationId"] = $configs['relayStationId'];
     $result = send_data($data, $configs['relayUrl']);
   }
 
@@ -24,7 +24,7 @@ if (($_SERVER["REQUEST_METHOD"] == "POST") && (trim(trim($_POST["upkey"],"'"),'"
 } elseif($_GET["test-upload"] == 'y' && $configs['testUrl']) { // send test message to other data collection services
 	$json = '{"Time": "2019-4-4T4:00:08", "A0": 284.77, "A1": 378, "A2": 381, "A3": 390, "A4": 365, "A5": 340, "A6": 385, "A7": 372, "A8": 347, "A9": 359, "A10": 374, "A11": 366, "A12": 356, "A13": 380, "A14": 382, "A15": 358, "D2": 0, "D3": 1, "D4": 1, "D5": 1, "D6": 1, "D7": 1, "D8": 1, "D9": 1, "D10": 1, "D11": 1, "D12": 1, "D13": 1, "D14": 0, "D15": 0, "D16": 0, "D17": 0, "D18": 0, "D19": 0, "D20": 1, "D21": 1, "D22": 0, "D23": 0, "D24": 0, "D25": 0, "D26": 0, "D27": 0, "D28": 0, "D29": 0, "D30": 0, "D31": 0, "D32": 0, "D33": 0, "D34": 0, "D35": 0, "D36": 0, "D37": 0, "D38": 0, "D39": 0, "D40": 0, "D41": 0, "D42": 0, "D43": 0, "D44": 0, "D45": 0, "D46": 0, "D47": 0, "D48": 0, "D49": 0, "D50": 0, "D51": 0, "D52": 0, "D53": 0}';
 	$data = json_decode($json, true);
-	$data["stationId"] = 'TEST1';
+	$data["stationId"] = $configs['testStationId'];
 	$result = send_data($data, $configs['testUrl']);
 	echo $result;
 } else {
